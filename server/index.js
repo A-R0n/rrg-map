ea013e18daaa98bcb1dbdfc89f865cb3228d3df3
Aaron Estes <aestesc@gmail.com>
2020-12-30 11:07:53 -0600

    db config ssl params

--- server/index.js
+++ server/index.js
@@ -3,9 +3,7 @@ const express = require("express");
 const { json } = require("body-parser");
 const cors = require("cors");
 const massive = require("massive");
-const session = require("express-session");
 
-// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const app = express();
 
 const {getParkingLotId, getAllBasicRouteInfo} = require("./Controllers/main");
@@ -15,32 +13,26 @@ const port = process.env.SERVER_PORT || 8081;
 app.use(json());
 app.use(cors());
 
-app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 14 * 24 * 60 * 60 * 1000
-      }
-    })
-  );
+const config = {
+  connectionString: process.env.CONNECTION_STRING,
+  // i am opting out of MITM protection by setting this to false
+  ssl: {
+    rejectUnauthorized: false
+  }
+};
 
-massive(process.env.CONNECTION_STRING)
+
+massive(config)
   .then(dbInstance => {
+    // console.log("read this dbInstance:", dbInstance);
     app.set("db", dbInstance);
   })
   .catch(err => console.log(err));
 
-  process.on('uncaughtException', function (err) {
-    console.log(err);
-});
-
-// app.get(`/api/routes`, getAllRoutes);
 app.get(`/api/routes`, getAllBasicRouteInfo);
 app.get(`/api/parkinglot/:parkinglotid`, getParkingLotId);
 
-app.use(express.static(`${__dirname}/../build`))
+app.use(express.static(`${__dirname}/../build`));
 
 app.listen(port, () => {
     console.log(`Nothin can stop me im All the wayyyy upppp: ${port}`);

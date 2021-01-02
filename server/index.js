642825edd70c1bed88508a477d96bb3d7dbdde22
Aaron Estes <aestesc@gmail.com>
2021-01-02 04:19:31 -0600

    make global api random

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ const localCSP = csp.getCSP(cspPolicy);
 
 app.use(globalCSP);
 
-app.get('/', (req, res) => {
+app.get('/random', (req, res) => {
   res.send('Using global content security policy!');
 });
 app.get('/local', localCSP, (req, res) => {

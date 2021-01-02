c73292cfb1857e37da4fa20e702cf0329a3a79ec
Aaron Estes <aestesc@gmail.com>
2021-01-02 02:32:08 -0600

    sec test

--- server/index.js
+++ server/index.js
@@ -2,32 +2,32 @@ require("dotenv").config();
 const express = require("express");
 const { json } = require("body-parser");
 const cors = require("cors");
-const massive = require("massive");
+// const massive = require("massive");
 
 const app = express();
 
-const {getParkingLotId, getAllBasicRouteInfo} = require("./Controllers/main");
+// const {getParkingLotId, getAllBasicRouteInfo} = require("./Controllers/main");
 
 const port = process.env.SERVER_PORT || 8081;
 
 app.use(json());
 app.use(cors());
 
-const dbConfig = {
-  connectionString: process.env.CONNECTION_STRING,
-  ssl: {
-    rejectUnauthorized: false
-  }
-};
+// const dbConfig = {
+//   connectionString: process.env.CONNECTION_STRING,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// };
 
-massive(dbConfig)
-  .then(dbInstance => {
-    app.set("db", dbInstance);
-  })
-  .catch(err => console.log(err));
+// massive(dbConfig)
+//   .then(dbInstance => {
+//     app.set("db", dbInstance);
+//   })
+//   .catch(err => console.log(err));
 
-app.get(`/api/routes`, getAllBasicRouteInfo);
-app.get(`/api/parkinglot/:parkinglotid`, getParkingLotId);
+// app.get(`/api/routes`, getAllBasicRouteInfo);
+// app.get(`/api/parkinglot/:parkinglotid`, getParkingLotId);
 
 app.use(express.static(`${__dirname}/../build`));
 

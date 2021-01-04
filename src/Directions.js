4eec50061d1095384d554c5e7c177273e1cfed7b
Aaron Estes <aestesc@gmail.com>
2021-01-04 13:03:57 -0600

    Remove unused

--- src/Directions.js
+++ src/Directions.js
@@ -36,9 +36,6 @@ export const Directions = (fields) => {
               travelMode: "DRIVING"
             }}
             callback={directionsCallback}
-            onLoad={directionsService => {
-                console.log('DirectionsService onLoad directionsService: ', directionsService)
-              }}
               // optional
             onUnmount={(ds) => {
                 console.log('DirectionsService unmount: ', ds);

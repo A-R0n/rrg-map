a0825f6dba126555f2cd7583f2048974047de71e
Aaron Estes <aestesc@gmail.com>
2021-01-26 17:58:59 -0600

    Include textLength and lengthAdjust properties

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -5,7 +5,7 @@ export const RedRiverGorge = () => {
     return (
         <div className="outerSvgHeader">
             <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-                <text id="rrgTitleText" x="5" y="15" >Red River Gorge</text>
+                <text id="rrgTitleText" x="5" y="15" textLength="7em" lengthAdjust="spacingAndGlyphs">Red River Gorge</text>
             </svg>
         </div>
     )

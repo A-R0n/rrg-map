c19ac057c06466c22cc40bd9004553668b15d059
Aaron Estes <aestesc@gmail.com>
2021-01-26 17:52:13 -0600

    Adjust title size

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -4,9 +4,8 @@ import './RedRiverGorge.css';
 export const RedRiverGorge = () => {
     return (
         <div className="outerSvgHeader">
-            {/* <h1>Red River Gorge</h1> */}
             <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg">
-                <text x="5" y="15">Red River Gorge</text>
+                <text id="rrgTitleText" x="5" y="15" textLength="7em" lengthAdjust="spacingAndGlyphs">Red River Gorge</text>
             </svg>
         </div>
     )

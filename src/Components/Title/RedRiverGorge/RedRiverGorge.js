d0d246e39ee8a405f5dfc7bb3095077cdf4859eb
Aaron Estes <aestesc@gmail.com>
2021-01-26 17:57:30 -0600

    Do not preserve the aspect ratio for svg text

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -4,8 +4,8 @@ import './RedRiverGorge.css';
 export const RedRiverGorge = () => {
     return (
         <div className="outerSvgHeader">
-            <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg">
-                <text id="rrgTitleText" x="5" y="15" textLength="7em" lengthAdjust="spacingAndGlyphs">Red River Gorge</text>
+            <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+                <text id="rrgTitleText" x="5" y="15" >Red River Gorge</text>
             </svg>
         </div>
     )

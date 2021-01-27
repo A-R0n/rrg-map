abd5ec31afe90fe0d3131407123ff9c93d1730f9
Aaron Estes <aestesc@gmail.com>
2021-01-27 00:04:07 -0600

    no preserve aspect ratio

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -4,7 +4,7 @@ import './RedRiverGorge.css';
 export const RedRiverGorge = () => {
     return (
         <div className="outerSvgHeader">
-            <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+            <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
                 <rect x="1" y="1" width="298" height="18"
                 fill="none" stroke="pink" stroke-width="2"/>
                 <text id="rrgTitleText" x="5" y="15">Red River Gorge</text>

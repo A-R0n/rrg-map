b58f0cfa3e78ea8b26fa0f6a6efd8a8cd8e977d7
Aaron Estes <aestesc@gmail.com>
2021-01-27 00:07:04 -0600

    textlength

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -7,7 +7,7 @@ export const RedRiverGorge = () => {
             <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
                 <rect x="1" y="1" width="298" height="18"
                 fill="none" stroke="pink" stroke-width="2"/>
-                <text id="rrgTitleText" x="5" y="15">Red River Gorge</text>
+                <text id="rrgTitleText" x="5" y="15" textLength="8em">Red River Gorge</text>
             </svg>
         </div>
     )

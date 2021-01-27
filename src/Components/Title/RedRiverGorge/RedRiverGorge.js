3ba0a8fbf99419fc0cdbc8fe793653241e392e38
Aaron Estes <aestesc@gmail.com>
2021-01-27 00:17:48 -0600

    text length 9em

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -7,7 +7,7 @@ export const RedRiverGorge = () => {
             <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
                 <rect x="1" y="1" width="298" height="18"
                 fill="none"/>
-                <text id="rrgTitleText" x="5" y="15" textLength="10em">Red River Gorge</text>
+                <text id="rrgTitleText" x="5" y="15" textLength="9em">Red River Gorge</text>
             </svg>
         </div>
     )

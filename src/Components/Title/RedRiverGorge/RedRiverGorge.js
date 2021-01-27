067558d2889cf2fb7582511b2c5b0ede5c00a1a6
Aaron Estes <aestesc@gmail.com>
2021-01-27 12:47:39 -0600

    Remove comments

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -4,10 +4,13 @@ import './RedRiverGorge.css';
 export const RedRiverGorge = () => {
     return (
         <div className="outerSvgHeader">
-            <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-                <rect x="1" y="1" width="298" height="18"
+            <svg viewBox="0 0 120 16" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+                <rect id="rrg_title_rect" x="1" y="1" width="82" height="14"
                 fill="none"/>
-                <text id="rrgTitleText" x="5" y="15" textLength="9em">Red River Gorge</text>
+                <text id="rrgTitleText" x="3" y="12" textLength="80" lengthAdjust="spacingAndGlyphs">Red River Gorge</text>
+                <rect id="emoji_title_rect" x="86" y="3" width="10" height="9"
+                fill="none"/>
+                <text id="emojiTitleText" x="86" y="10" textLength="10" lengthAdjust="spacingAndGlyphs">🧗</text>
             </svg>
         </div>
     )

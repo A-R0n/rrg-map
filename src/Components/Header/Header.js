64a4905f66983706bc9a06e253ab4807fc0d4fb9
Aaron Estes <aestesc@gmail.com>
2021-01-26 23:55:18 -0600

    min width lol

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Title} from '../Title/Title.js';
-import './Header.css';
 
 export const Header = () => {
     return (
--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Title.css';
 import {RedRiverGorge} from './RedRiverGorge/RedRiverGorge.js';
 
 export const Title = () => {

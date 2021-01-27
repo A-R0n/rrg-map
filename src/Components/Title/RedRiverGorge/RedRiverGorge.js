a002ada72518cee2e7b62d0771e52467c6da67b8
Aaron Estes <aestesc@gmail.com>
2021-01-27 00:16:40 -0600

    define width for svg text in media query

--- src/Components/Title/RedRiverGorge/RedRiverGorge.js
+++ src/Components/Title/RedRiverGorge/RedRiverGorge.js
@@ -6,7 +6,7 @@ export const RedRiverGorge = () => {
         <div className="outerSvgHeader">
             <svg viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
                 <rect x="1" y="1" width="298" height="18"
-                fill="none" stroke="pink" stroke-width="2"/>
+                fill="none"/>
                 <text id="rrgTitleText" x="5" y="15" textLength="10em">Red River Gorge</text>
             </svg>
         </div>
